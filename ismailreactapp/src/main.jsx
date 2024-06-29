6a5f84a390a3f4ac7cfc30a2660c58ef1b8435e4
Ismail-Hanif <161356594+Ismail-Hanif@users.noreply.github.com>
2024-06-29 14:42:53 +0500

    Add files via upload

---
+++ ismailreactapp/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // <div>
+  //   <h1>Hello World</h1>
+  //   <p>
+  //   <ipsum>Lorem ipsum dolor sit amet.</ipsum>
+    
+    
+  //   </p>
+  //   </div>
+)
